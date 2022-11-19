75812aaaf011d5d9354e727971c9fbd0c7612c36
Josell Vibar <vibarjosell@gmail.com>
2022-11-19 11:55:05 +0800

    add svg directorya and add 2 divs for the brand name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,8 @@
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     <!-- {{ config('app.name', 'Laravel') }} -->
-                    EJAY|WEBSITE
+                    <div><img src="/svg/tanjiro.svg" alt=""></div>
+                    <div>EJAYWEBSITE</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
