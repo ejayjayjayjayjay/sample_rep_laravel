b2a6c59d3ca824e785396472ffab1324209f2941
Josell Vibar <vibarjosell@gmail.com>
2022-11-19 13:12:55 +0800

    add user title,user description , website url and post images placeholder

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,11 +25,19 @@
         </div>
         <div class="col-9 pt-5">
             <div><h1>EjayVibar</h1></div>
-            <div class="d-flex">
+            <div class="d-flex align-items-center">
                 <div style="padding-right: 4%;"><strong>100</strong> posts</div>
                 <div style="padding-right: 4%;"><strong>20k</strong> followers</div>
                 <div style="padding-right: 4%;"><strong>200</strong> following</div>
             </div>
+            <div class="pt-4 fw-bold">User Title</div>
+            <div>User Description</div>
+            <div><a>Website Url</a></div>
+        </div>
+        <div class="row">
+            <div class="col-4"><img src="https://source.unsplash.com/1600x900/?computers" alt=""></div>
+            <div class="col-4"><img src="https://source.unsplash.com/1600x900/?landscape" alt=""></div>
+            <div class="col-4"><img src="https://source.unsplash.com/1600x900/?dogs" alt=""></div>
         </div>
     </div>
 </div>
