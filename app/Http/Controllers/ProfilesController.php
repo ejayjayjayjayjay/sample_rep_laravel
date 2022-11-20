b6065b5b766d11820f3ae739c00eafb899151cd1
Josell Vibar <vibarjosell@gmail.com>
2022-11-20 11:49:13 +0800

    test die and dump User::find(user);

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\user; // this will not work
 use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
 {
-    public function index()
+    public function index($user)
     {
+        dd(User::find($user));
         return view('home');
     }
 }
