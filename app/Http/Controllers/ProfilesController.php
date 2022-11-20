669cd4266c13bed4798df9576e612fd7cf267146
Josell Vibar <vibarjosell@gmail.com>
2022-11-20 11:53:22 +0800

    profile/1 working assign User::find(user) to a variable user

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\user; // this will not work
+use App\Models\user;
 use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
 {
     public function index($user)
     {
-        dd(User::find($user));
+        $user = User::find($user);
+
         return view('home');
     }
 }
